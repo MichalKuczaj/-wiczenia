8fe90e1e4b13bfac47ed40f0a821bf06d12bd8a4
MichalKuczaj <48965403+MichalKuczaj@users.noreply.github.com>
2019-11-12 18:24:52 +0100

    Trójka Pitagorejska

---
+++ TrójkaPitagorejska.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrójkaPitagorejska
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Program sprawdza czy podane cyfry a, b, c są trójką pitagorejską");
+            Console.ReadLine();
+            Console.WriteLine("Podaj a. ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b. ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj c. ");
+            c = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
